4b5d3b568533512c93e29b3d9d3e2d531e36be78
Kevin Ruhl <kevin.ruhl92@gmail.com>
2024-06-12 14:42:51 -0300

    create POST route handler to fetch from streamOpenAI service

---
+++ src/app/api/route.ts
@@ -0,0 +1,19 @@
+import { streamOpenAI } from "@/lib/utils";
+import { type RequestBody } from "@/types/type";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function POST(req: Request) {
+  try {
+    const { messages, model, apiKey } = (await req.json()) as RequestBody;
+
+    const stream = await streamOpenAI(messages, model, apiKey);
+
+    return new Response(stream);
+  } catch (error) {
+    console.error(error);
+    return new Response("Error", { status: 500 });
+  }
+}
